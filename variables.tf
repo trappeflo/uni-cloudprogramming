e227e606586655b7d520389d97d9b8dedec950f6
trappeflo <zimmermann99fl@gmail.com>
2026-06-29 23:24:15 +0200

    update variables

--- variables.tf
+++ variables.tf
@@ -1,36 +1,36 @@
 # https://developer.hashicorp.com/terraform/language/manage-sensitive-data
 # https://developer.hashicorp.com/terraform/language/providers
 variable "cloudflare_api_token" {
-  description = "Cloudflare API Token für DNS-Verwaltung"
+  description = "Cloudflare API Token"
   type        = string
   sensitive   = true
 }
 
 variable "cloudflare_zone_id" {
-  description = "Zone ID der Domain trappeonline.xyz in Cloudflare"
+  description = "Cloudflare Zone ID"
   type        = string
 }
 
 variable "location" {
-  description = "Azure Region für die Resource Group"
+  description = "Azure Region"
   type        = string
   default     = "westeurope"
 }
 
 variable "resource_group_name" {
-  description = "Name der Azure Resource Group"
+  description = "Azure Resource Group"
   type        = string
   default     = "rg-cloud-programming"
 }
 
 variable "static_web_app_name" {
-  description = "Name der Azure Static Web App"
+  description = "Azure Static Web App"
   type        = string
   default     = "swa-cloud-programming"
 }
 
 variable "dns_record_name" {
-  description = "Subdomain für die Website"
+  description = "Subdomain"
   type        = string
   default     = "cloud"
 }
